90dde1973d42021cd44401aa45e90e25f7ca0a03
Lizaveta Sasinovich <liza_sasinovich@mail.ru>
2018-05-07 20:51:51 +0300

    Update tests

--- client/components/full-film-item/full-film-item.test.jsx
+++ client/components/full-film-item/full-film-item.test.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { FullFilmItem } from './full-film-item';
 
+const testData = {
+  title: "Title",
+  posterUrl: "PosterUrl",
+  genre: "Genre",
+  additional: "Additional",
+  year: 1900,
+  rating: 4.6,
+  duratin: 123,
+  description: "Description"
+}
+
 describe('<FullFilmItem/>', function() {
   it('should render FullFilmItem and match snapshot', function() {
       const wrapper = shallow(<FullFilmItem />);
       expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render FullFilmItem and match snapshot', function() {
+    const wrapper = shallow(<FullFilmItem props={testData}/>);
+    expect(wrapper).toMatchSnapshot();
   });
 });
--- client/components/results-body-error-boundary/results-body-error-boundary.test.jsx
+++ client/components/results-body-error-boundary/results-body-error-boundary.test.jsx
@@ -3,13 +3,13 @@ import { ResultsBodyErrorBoundary } from './results-body-error-boundary';
 
 describe('<ResultsBodyErrorBoundary/>', function() {
   it('should render ResultsBodyErrorBoundary and match snapshot', function() {
-      const wrapper = shallow(<ResultsBodyErrorBoundary fullItem={false}/>);
+      const wrapper = shallow(<ResultsBodyErrorBoundary children={<div>Hello</div>} />);
 
       expect(wrapper).toMatchSnapshot();
   });
 
   it('should render ResultsBodyErrorBoundary with Full Film Item', function() {
-    const wrapper = shallow(<ResultsBodyErrorBoundary />);
+    const wrapper = shallow(<ResultsBodyErrorBoundary children={<div>Hello</div>}/>);
     wrapper.setState({hasError: true});
 
     expect(wrapper).toMatchSnapshot();
