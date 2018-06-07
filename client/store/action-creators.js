c9a466cb837ba3e81691c686675ca81e567c2ba2
Igor Rumyancev <irumyancev@epam.com>
2018-06-07 15:35:14 +0300

    Add eslint

--- client/store/action-creators.js
+++ client/store/action-creators.js
@@ -1,172 +1,156 @@
+import { call, put, takeLatest, fetch } from 'redux-saga/effects';
 import { ACTIONS, SORT, SEARCH } from '../constants/app-constants';
-import { call, put, all, takeLatest } from 'redux-saga/effects';
 
+export const setEmptyResults = () => ({
+  type: ACTIONS.SET_EMPTY_RESULTS,
+});
 
-export const setEmptyResults = () => {
-    return {
-        type: ACTIONS.SET_EMPTY_RESULTS
-    }
-}
+export const setSearchURL = searchURL => ({
+  type: ACTIONS.SET_SEARCH_URL,
+  searchURL,
+});
 
-export const setSearchURL = (searchURL) => {
-    return {
-        type: ACTIONS.SET_SEARCH_URL,
-        searchURL: searchURL
-    }
-}
+export const switchSort = (sort, results) => ({
+  type: ACTIONS.SWITCH_SORT,
+  sort,
+  results,
+});
 
-export const switchSort = (sort, results) => {
-    return {
-        type: ACTIONS.SWITCH_SORT,
-        sort: sort,
-        results: results
-    }
-}
+export const getMoviesByGenresAction = results => ({
+  type: ACTIONS.GET_MOVIES_BY_GENRES,
+  results,
+});
 
-export const getMoviesByGenresAction = (results) => {
-    return {
-        type: ACTIONS.GET_MOVIES_BY_GENRES,
-        results: results
-    }
-}
+export const fullFilmLoad = json => ({
+  type: ACTIONS.FULL_FILM_LOAD,
+  filmData: json,
+});
 
-export const fullFilmLoad = (json) => {
-    return {
-        type: ACTIONS.FULL_FILM_LOAD,
-        filmData: json,
-    };
-}
+export const returnToMainPage = () => ({
+  type: ACTIONS.RETURN_TO_MAIN_PAGE,
+});
 
-export const returnToMainPage = () => {
-    return {
-        type: ACTIONS.RETURN_TO_MAIN_PAGE
-    };
-}
+export const updateInputValue = value => ({
+  type: ACTIONS.UPDATE_INPUT_VALUE,
+  value,
+});
 
-export const updateInputValue = (value) => {
-    return {
-        type: ACTIONS.UPDATE_INPUT_VALUE,
-        value: value
-    };
-}
+export const resetInputValue = () => ({
+  type: ACTIONS.RESET_INPUT_VALUE,
+});
 
-export const resetInputValue = () => {
-    return {
-        type: ACTIONS.RESET_INPUT_VALUE
-    };
-}
+export const searchHandler = (results, sortBy, searchBy, input) => ({
+  type: ACTIONS.SEARCH,
+  results,
+  sortBy,
+  searchBy,
+  input,
+});
 
-export const searchHandler = (results, sortBy, searchBy, input) => {
-    return {
-        type: ACTIONS.SEARCH,
-        results: results,
-        sortBy: sortBy,
-        searchBy: searchBy,
-        input: input
-    };
-}
+export const switchSearch = search => ({
+  type: ACTIONS.SWITCH_SEARCH,
+  search,
+});
 
-export const switchSearch = (search) => {
-    return {
-        type: ACTIONS.SWITCH_SEARCH,
-        search: search
-    };
-}
+export const fetchMovies = (sort, search, inputValue) => ({
+  type: ACTIONS.FETCH_MOVIES,
+  payload: {
+    sort,
+    search,
+    inputValue,
+  },
+});
 
-export const fetchMovies = (sort, search, inputValue) => {
-    console.log('here');
-    return {
-        type: ACTIONS.FETCH_MOVIES,
-        payload: {
-            sort: sort, 
-            search: search, 
-            inputValue: inputValue
-        }
-    }
-}
+export const fetchMoviesByGenres = genre => ({
+  type: ACTIONS.FETCH_MOVIES_BY_GENRES,
+  payload: {
+    genre,
+  },
+});
 
-export const fetchMoviesByGenres = (genre) => {
-    return {
-        type: ACTIONS.FETCH_MOVIES_BY_GENRES,
-        payload: {
-            genre: genre
-        }
-    }
-}
+export const fetchMoviesById = id => ({
+  type: ACTIONS.FETCH_MOVIES_BY_ID,
+  payload: {
+    id,
+  },
+});
 
-export const fetchMoviesById = (id) => {
-    return {
-        type: ACTIONS.FETCH_MOVIES_BY_ID,
-        payload: {
-            id: id
-        }
-    }
-}
-
-export const switchSortAsyncAction = (sort, stateSort, inputValue, search) => {
-    return {
-        type: ACTIONS.SWITCH_SORT_ASYNC_ACTION,
-        payload: {
-            sort: sort, 
-            stateSort: stateSort, 
-            inputValue: inputValue, 
-            search: search
-        }
-    }
-}
+export const switchSortAsyncAction = (sort, inputValue, search) => ({
+  type: ACTIONS.SWITCH_SORT_ASYNC_ACTION,
+  payload: {
+    sort,
+    inputValue,
+    search,
+  },
+});
 
 export function* getMovie(action) {
-    const { sort, search, inputValue } = action.payload;
-    const value = inputValue || '';
-    const response = yield call(fetch, `http://react-cdp-api.herokuapp.com/movies?sortBy=${SORT[sort]}&sortOrder=desc&search=${value}&searchBy=${SEARCH[search]}&limit=12`);
-    const results = yield response.json();
-  
-    yield put(searchHandler(results, sort, search, value));
+  const { sort, search, inputValue } = action.payload;
+  const value = inputValue || '';
+  const response =
+  yield call(fetch, `http://react-cdp-api.herokuapp.com/movies? \
+    sortBy=${SORT[sort]} \
+    &sortOrder=desc&search=${value} \
+    &searchBy=${SEARCH[search]} \
+    &limit=12`);
+  const results = yield response.json();
+
+  yield put(searchHandler(results, sort, search, value));
 }
 
 export function* watchFetchMovies() {
-    yield takeLatest(ACTIONS.FETCH_MOVIES, getMovie);
+  yield takeLatest(ACTIONS.FETCH_MOVIES, getMovie);
 }
 
 export function* getMoviesByGenres(action) {
-    const genres = action.payload.genre;
+  const genres = action.payload.genre;
 
-    const response = yield call(fetch, `http://react-cdp-api.herokuapp.com/movies?search=${genres}&searchBy=genres&limit=12`);
-    const results = yield response.json();
-  
-    yield put(getMoviesByGenresAction(results));
+  const response = yield call(fetch, `http://react-cdp-api.herokuapp.com/movies? \
+    search=${genres} \
+    &searchBy=genres \
+    &limit=12`);
+  const results = yield response.json();
+
+  yield put(getMoviesByGenresAction(results));
 }
 
 export function* watchFetchMoviesByGenres() {
-    yield takeLatest(ACTIONS.FETCH_MOVIES_BY_GENRES, getMoviesByGenres);
+  yield takeLatest(ACTIONS.FETCH_MOVIES_BY_GENRES, getMoviesByGenres);
 }
 
 export function* fullLoad(action) {
-    const id = action.payload.id;
+  const id = action.payload.id;
 
-    const response = yield call(fetch, `http://react-cdp-api.herokuapp.com/movies/${id}`);
-    const results = yield response.json();
-    if(results.id) {
-        yield put(fullFilmLoad(results));
-        yield put(fetchMoviesByGenres(results.genres[0]));
-    } else {
-        window.location.pathname = '/not_found';
-    }
+  const response = yield call(fetch, `http://react-cdp-api.herokuapp.com/movies/${id}`);
+  const results = yield response.json();
+  if (results.id) {
+    yield put(fullFilmLoad(results));
+    yield put(fetchMoviesByGenres(results.genres[0]));
+  } else {
+    window.location.pathname = '/not_found';
+  }
 }
 
 export function* watchFetchMoviesById() {
-    yield takeLatest(ACTIONS.FETCH_MOVIES_BY_ID, fullLoad);
+  yield takeLatest(ACTIONS.FETCH_MOVIES_BY_ID, fullLoad);
 }
 
 export function* switchSortAction(action) {
-    const { sort, stateSort, inputValue, search } = action.payload;
+  const {
+    sort, inputValue, search,
+  } = action.payload;
 
-    const response = yield call(fetch, `http://react-cdp-api.herokuapp.com/movies?sortBy=${SORT[sort]}&sortOrder=desc&search=${inputValue}&searchBy=${SEARCH[search]}&limit=12`);
-    const results = yield response.json();
-  
-    yield put(switchSort(sort, results));
+  const response = yield call(fetch, `http://react-cdp-api.herokuapp.com/movies? \
+    sortBy=${SORT[sort]} \
+    &sortOrder=desc& \
+    search=${inputValue} \
+    &searchBy=${SEARCH[search]} \
+    &limit=12`);
+  const results = yield response.json();
+
+  yield put(switchSort(sort, results));
 }
 
 export function* watchSwitchSort() {
-    yield takeLatest(ACTIONS.SWITCH_SORT_ASYNC_ACTION, switchSortAction);
+  yield takeLatest(ACTIONS.SWITCH_SORT_ASYNC_ACTION, switchSortAction);
 }
